c774ae17c74bbf071e7c8e658ac5ad09fcd5ad3e
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-12 01:50:07 +0530

    sql-node-application

---
+++ nodejs/express_mysql1/server.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'myuser',
+    password:'mypass',
+    database: 'testdb' 
+});
+
+//this wil have 2 params query and the callback function 
+connection.query(
+    `SELECT * FROM test1`,
+     function (err, rows, cols) {
+        //if (err)  console.log(err);
+        console.log(rows);
+        console.log(cols);
+    }
+)
