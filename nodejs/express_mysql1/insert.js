d5acaeea8f8ad91fa7664dc0b012635de2a9385d
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-12 02:08:04 +0530

    Script to insert data into a table

---
+++ nodejs/express_mysql1/insert.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+
+const insert = {
+    name : process.argv[2] , 
+    age :  parseInt(process.argv[3]),
+    city : process.argv[4]
+}
+
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'myuser',
+    pass : 'mypass',
+    database : 'testdb'
+})
+
+connection.query(
+    `INSERT INTO test1 (name , age , city ) VALUES (
+        '${insert.name}',
+        '${insert.age}',
+        '${insert.city}'
+    )`,
+    (err , results) => {
+        if(err)
+        console.log(err);
+        else
+        console.log(results);
+        console.log(`values inserted`)
+    } 
+)
