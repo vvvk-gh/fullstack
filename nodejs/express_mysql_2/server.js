a3cf72d428ffc6f03b4162d990bd289e433d50d7
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-12 23:01:42 +0530

    added comments

--- nodejs/express_mysql_2/server.js
+++ nodejs/express_mysql_2/server.js
@@ -5,38 +5,12 @@ const db = require('./db')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+//set the view engine
 app.set('view engine' , 'hbs');
 
- app.get('/' , (req, res)=> {
-    //gets the data from database
-    db.getAllPersons()
-    .then((persons) => {
-        console.log(persons)
-        res.render('persons.hbs' , {
-            heading : 'Persons Table',
-            persons
-        })   
-    })
-    .catch(err => console.log(err))
- })
+// app routes
+app.use('/add' , require('./routes/add_persons'));
+app.use('/persons' , require('./routes/persons'))
 
- app.get('/add' , (req, res)=> {     
-    res.render('add_persons.hbs' , {
-        heading : 'Add Persons to Persons Table',
-    })
-})
-
-app.post('/add' , (req, res)=> {
-    let name = req.body.name;
-    let age = parseInt(req.body.age);
-    let city = req.body.city;
-
-    db.add_person(name,age,city)
-    .then(()=> {
-        console.log('Values added');
-        res.redirect('/')
-        })
-    .catch((err)=> console.log(err));
-})
-
+// app server
 app.listen(4444 ,  ()=> {console.log(`App running at http://localhost:4444/`)})
