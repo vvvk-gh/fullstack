08fc88046ca10bf4fba003572218b5dba65297db
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-15 00:46:52 +0530

    seeding the intail data

---
+++ nodejs/node-sequelize/seed.js
@@ -0,0 +1,35 @@
+const {db , Student , Batch , Course , Season , Center} = require('./model');
+
+Season.bulkCreate([
+    {id :'S', name: `Summer`},
+    {id :'F', name: 'Fall'},
+    {id :'W', name:'Winter'},
+    {id :'P', name:'Spring'},
+    ] , 
+{
+    ignoreDuplicates : true
+});
+
+Center.bulkCreate([
+    {id :'HC', name: `Hitech City`, city:'Hyderabad'},
+    {id :'DN', name: 'Dilshuk Nagar', city:'Delhi'},
+    {id :'TH', name:'Time Hospital', city:'Kolkata'},
+    {id :'OL', name:'Online', city:'Online'},
+    ] , 
+{
+    ignoreDuplicates : true
+});
+
+Course.bulkCreate([
+    {id :'WD', name: `Web Developement`},
+    {id :'AB', name: 'Android Bootcamp'},
+    {id :'AI', name:'Artifical Intelligence'},
+    {id :'IP', name:'Interview Prepartion'},
+    ] , 
+{
+    ignoreDuplicates : true
+});
+
+db.sync()
+.then(() => console.log('Values added into the database'))
+.catch((err) => console.log(err) )
