8da7510a6e56b43d0c24574f96058bae0d2f2498
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-15 23:09:43 +0530

    updated the schema for batches

--- nodejs/node-sequelize/model.js
+++ nodejs/node-sequelize/model.js
@@ -6,78 +6,78 @@ const db = new Sequelize('sampledb' , 'sampleuser' , 'samplepass', {
     dialect : 'mysql'
 })
 
-const Student = db.define('students' , {
-    name : {
-        type: DataTypes.STRING(40),
-        allowNull : false,            
-    },
-    age : {
-        type : DataTypes.INTEGER(2),
-        allowNull : false,
-        defaultValue : -1,
-    }
-}) 
-
-
-const Course = db.define('courses' , {
-    id : {
-        type : DataTypes.STRING(2),
-        allowNull: false,
-        primaryKey: true,
-    },
-    name : {
-        type: DataTypes.STRING(40),
-        allowNull : false,            
-    }
-}) 
-
-const Season = db.define('seasons' , {
-    id : {
-        type : DataTypes.STRING(1),
-        allowNull: false,
-        primaryKey: true,
-    },
-    name : {
-        type: DataTypes.STRING(40),
-        allowNull : false,            
-    }
-})
-
-const Center = db.define('centers' , {
+//define a cousre
+const Course = db.define('course' ,{
     id:{
         type : DataTypes.STRING(2),
-        allowNull : false,  
         primaryKey: true,
     },
-    name : {
+    name:{
         type: DataTypes.STRING(40),
-        allowNull : false,            
+    }
+
+})
+
+
+//defina a center
+
+const Center = db.define('center' ,{
+    id:{
+        type : DataTypes.STRING(2),
+        primaryKey: true,
+    },
+    name:{
+        type: DataTypes.STRING(15),
+        allowNull:false,
     },
     city : {
-        type: DataTypes.STRING(40),
-        allowNull : false,            
-    },
+        type:DataTypes.STRING(19),
+        allowNull:false
+    }
 })
 
-const Batch = db.define('batches', {
-    id:{
+
+//define a batch
+
+const Batch = db.define('batch' ,{
+    code:{
         type : DataTypes.STRING(8),
-        allowNull : false,  
         primaryKey: true,
     },
-    year : DataTypes.STRING(4),
+    year: DataTypes.INTEGER(4),
     start : DataTypes.DATE,
-    end : DataTypes.DATE,
-}) 
+    end:DataTypes.DATE
+})
+
+//define a season 
+
+const Season = db.define('season' ,{
+    id:{
+        type : DataTypes.STRING(2),
+        primaryKey:true,
+    },
+    name:{
+        type: DataTypes.STRING(10),
+        allowNull: false,
+    }
+
+})
+
+//assoociations
+
+Batch.belongsTo(Course)
+Batch.belongsTo(Center)
+Batch.belongsTo(Season)
+
+Course.hasMany(Batch)
+Season.hasMany(Batch)
+Center.hasMany(Batch)
+//syncing
+db.sync({alter:true})
+    .then(()=> console.log('Database Synchronized'))
+    .catch((E)=> console.log(E))
 
 
-Batch.belongsTo(Course);
-Batch.belongsTo(Season);
-Batch.belongsTo(Center);
-
-Course.hasMany(Batch);
-Season.hasMany(Batch);
-Center.hasMany(Batch);
-
-
-module.exports =  {db , Student , Batch , Course , Season , Center}
+module.exports = {
+ db, Center , Season ,  Course , Batch
+}
