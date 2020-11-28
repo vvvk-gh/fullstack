47bc33f74fd86f7581257c08242a04328b02a309
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-29 00:04:25 +0530

    mutiple insert into db

--- mongodb/src/write.js
+++ mongodb/src/write.js
@@ -3,25 +3,25 @@ const {MongoClient} = require('mongodb');
 const url = 'mongodb://localhost:27017';
 const DB_NAME = 'todoDb';
 
-async function writeTask() {
+async function writeTasks() {
     
 const client = await MongoClient.connect(url)
 const todoDb = client.db(DB_NAME)
 
 // creating a collection (table) in the database
 const todos = todoDb.collection('todos');
- 
-//task
-const todo  = {
-    'task' : 'do this task' , 
-    'owner':'manager' , 
-    'priority': '1' 
-};
-// adding data/task into the table
-const result = await todos.insertOne(todo);
-console.log(result);
 
+
+// adding data/tasks into the table
+const result = await todos.insertMany([
+    {task: 'a task for anyone', priority: 4},   
+    {task: 'something anyone can do', priority: 1},    
+    {task: 'someone should finish this', priority: 2},
+    {task: 'someone else should finish this', priority: 3}
+]);
+
+console.log(result);
 }
 
-writeTask()
+writeTasks()
 
