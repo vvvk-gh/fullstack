11792e3daaf5208d8b5170bbb850838b73f11558
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-28 17:56:10 +0530

    connection and creating db

---
+++ mongodb/src/connection.js
@@ -0,0 +1,19 @@
+const {MongoClient} = require('mongodb');
+
+
+// MongoDB url
+const url = 'mongodb://localhost:27017';
+
+//DB Name
+const dbName = 'todoDb';
+
+//Async IIFE's
+(async () =>{
+    const client = await MongoClient.connect(url);
+    
+    const todoDb = client.db(dbName);
+
+    console.log(todoDb);
+})()
+
+
