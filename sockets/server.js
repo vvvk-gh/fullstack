b12df3fdbdb7f5cb5fa6b99ae6b613330a3efef5
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-11-29 19:51:35 +0530

    server

---
+++ sockets/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const express = require('express');
+const app = express()
+
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io =  socketio(server);
+
+io.on('connection' , (socket)=>{
+    console.log('Client connected with client id  :' , socket.id);
+
+    socket.on('boom' , ()=>{
+        console.log('client with : '+socket.id+ '  boomed');
+    })
+
+})
+
+
+app.use('/' , express.static(__dirname + "/public"));
+
+
+server.listen(3434 , ()=> console.log('server running at http://localhost:3434'));
